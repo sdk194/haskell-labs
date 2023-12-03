e8bd88b05205b60853f4097e5b30b4d9fc7f1b17
Sean <118896924+sdk194@users.noreply.github.com>
2023-12-03 21:09:46 +0000

    Tried to do pretty printing

--- assignment/threes.hs
+++ assignment/threes.hs
@@ -1,6 +1,7 @@
 data TriTree t = Nil | BiNode t (TriTree t) (TriTree t) | TriNode t t (TriTree t) (TriTree t) (TriTree t) deriving (Eq, Ord, Show)
 
 myTree = TriNode 3 10 Nil (BiNode 6 Nil Nil) Nil
+myTree1 = TriNode 3 10 Nil (BiNode 6 Nil Nil) (TriNode 15 20 Nil (BiNode 18 Nil Nil) Nil)
 
 add :: Ord a => a -> TriTree a -> TriTree a
 add n Nil = BiNode n Nil Nil
@@ -38,3 +39,18 @@ height = \t ->
       (TriNode _ _ l m r) | height m >= height l && height m >= height r -> height m + 1
 
       (TriNode _ _ l m r) | height r >= height l && height r >= height m -> height r + 1
+
+preorder :: Show a => TriTree a -> [String]
+preorder Nil = ["Nil"]
+preorder (BiNode x l r) = [show x] ++ preorder l ++ preorder r 
+preorder (TriNode x y l m r) = [show x ++ "," ++ show y] ++ preorder l ++ preorder m ++ preorder r
+
+--test :: Int -> [a] -> String
+treeString n [] = "" 
+treeString n (h:t) 
+  |n == 0 = h ++ "\n" ++ treeString 1 t 
+  |n == 1 = h ++ "    " ++ treeString 2 t
+  |n == 2 = h ++ "    " ++ treeString 0 t
+
+prettyPrint :: Show a => TriTree a -> IO()
+prettyPrint x = putStrLn(treeString 0 (preorder x))
